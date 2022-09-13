4781f3151af988f9660c921d315a7819edd4d5d4
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-09-13 11:27:59 +0300

    Upd files

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,24 +2,29 @@ import PropTypes from 'prop-types';
 import { FriendListSection, List, Item, Status, Avatar, Name } from "./FriendList.styled";
 
 export const FriendList = ({ friends }) => {
-  const dataItems = friends.map(({id, avatar, name, isOnline }) => (
+  return (
+    <FriendListSection>
+      <List>
+        {friends.map(({id, avatar, name, isOnline }) => (
     <Item key={id}>
       <Status isOnline={isOnline}/>
       <Avatar src={avatar} alt="User avatar" width="48" />
       <Name>{name}</Name>
     </Item>
-  ));
-  return (
-    <FriendListSection>
-      <List>{dataItems}</List>
+  ))}
+      </List>
     </FriendListSection>
     
   )
 }
 
 FriendList.propTypes = {
-  key: PropTypes.number.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+  id: PropTypes.number.isRequired,
   isOnline: PropTypes.bool.isRequired,
   avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ),
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,27 +2,27 @@ import PropTypes from 'prop-types';
 import { StatisticsCard, Item, Title, StatList, Label, Percentage } from "./Statistics.styled"
 
 export const Statistics = ({ title, upload }) => {
-  const dataItems = upload.map(({ id, label, percentage }) => (
-          <Item key={id}>
-            <Label>{label}</Label>
-            <Percentage>{percentage}%</Percentage>
-          </Item>
-        ))
   return (
     <StatisticsCard>
       {title ? <Title>{title}</Title> : ''}
 
       <StatList>
-        {dataItems}
+        {upload.map(({ id, label, percentage }) => (
+          <Item key={id}>
+            <Label>{label}</Label>
+            <Percentage>{percentage}%</Percentage>
+          </Item>
+        ))}
       </StatList>
     </StatisticsCard>
   )
 }
 
 Statistics.propTypes = {
+  title: PropTypes.string,
   upload: PropTypes.arrayOf(
     PropTypes.exact({
-      key: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     }),
