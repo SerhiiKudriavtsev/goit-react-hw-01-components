9bc189c40bec7e6dc21d0ee7fc4b45f844310a35
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-09-15 14:42:24 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -41,12 +41,12 @@ export const TransactionHistory = ({items}) => {
   )
 }
 
-TransactionHistory.propType = {
+TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
-      amount: PropTypes.number.isRequired,
+      amount: PropTypes.string.isRequired,
       currency: PropTypes.string.isRequired,
     })
   )
